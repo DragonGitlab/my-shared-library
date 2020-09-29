07f4b30b7af61892ea358a3809ef0038e238f525
Hao.Han5 <Hao.Han5@homecreditcfc.cn>
2020-09-29 16:19:54 +0800

    add logs into shared library

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,6 +1,6 @@
 package vars
 
-def call(String name = 'human') {
+def call(String name = 'Human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello, ${name}."
